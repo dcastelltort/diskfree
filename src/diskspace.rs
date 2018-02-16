d5c45bca39871941289b2f4d16327093c38dd4c4
Damien Castelltort <dcastelltort@gmail.com>
2018-02-16 22:20:37 +0100

    misc(all): rustfmt pass

--- src/diskspace.rs
+++ src/diskspace.rs
@@ -1,24 +1,23 @@
-
 use libc;
 use std::mem::zeroed;
-use std::ffi::{CString};
+use std::ffi::CString;
 
-pub const HUMAN_BASE_1000 : u64 = 1000;
-pub const HUMAN_BASE_1024 : u64 = 1024;
-pub const HUMAN_BASE : u64 = 1;
+pub const HUMAN_BASE_1000: u64 = 1000;
+pub const HUMAN_BASE_1024: u64 = 1024;
+pub const HUMAN_BASE: u64 = 1;
 
 pub fn human_readable_size(value: u64, base: u64) -> String {
     let mut v = value;
     let mut unit = "B";
-    if base != HUMAN_BASE {        
+    if base != HUMAN_BASE {
         if value >= base * base * base {
-            v = value/base/base/base;
+            v = value / base / base / base;
             unit = "GB";
         } else if value >= base * base {
-            v = value/base/base;
+            v = value / base / base;
             unit = "MB";
         } else if value >= base {
-            v = value/base;
+            v = value / base;
             unit = "KB";
         }
     }
@@ -27,9 +26,9 @@ pub fn human_readable_size(value: u64, base: u64) -> String {
 
 #[test]
 fn test_human_readable_size() {
-    const KB : u64 = 1024;
-    const MB : u64 = 1024 * 1024;
-    const GB : u64 = 1024 * 1024 * 1024;
+    const KB: u64 = 1024;
+    const MB: u64 = 1024 * 1024;
+    const GB: u64 = 1024 * 1024 * 1024;
 
     assert_eq!(human_readable_size(0, HUMAN_BASE), "0B");
     assert_eq!(human_readable_size(0, HUMAN_BASE_1000), "0B");
@@ -52,17 +51,18 @@ fn test_human_readable_size() {
     assert_eq!(human_readable_size(GB, HUMAN_BASE_1024), "1GB");
 }
 pub struct DiskStats {
-    pub used : u64,
-    pub avail : u64,
-    pub capacity: u8
+    pub used: u64,
+    pub avail: u64,
+    pub capacity: u8,
 }
 
 impl DiskStats {
-    fn new(used : u64, avail: u64) -> DiskStats {
-        DiskStats{  used : used, 
-                    avail : avail,
-                    capacity : 100 - (avail*100/used) as u8
-                    }
+    fn new(used: u64, avail: u64) -> DiskStats {
+        DiskStats {
+            used: used,
+            avail: avail,
+            capacity: 100 - (avail * 100 / used) as u8,
+        }
     }
 }
 pub fn diskspace(mount_point: &str) -> Result<DiskStats, String> {
@@ -70,10 +70,15 @@ pub fn diskspace(mount_point: &str) -> Result<DiskStats, String> {
         let mountp = CString::new(mount_point).unwrap();
         let mut stats: libc::statvfs = zeroed();
         if libc::statvfs(mountp.as_ptr(), &mut stats) != 0 {
-            return Err(format!("Unable to retrieve mount point information {}", mount_point));
+            return Err(format!(
+                "Unable to retrieve mount point information {}",
+                mount_point
+            ));
         }
-        let disk_stats = DiskStats::new( stats.f_frsize * u64::from(stats.f_blocks), 
-                                         stats.f_frsize * u64::from(stats.f_bfree));
+        let disk_stats = DiskStats::new(
+            stats.f_frsize * u64::from(stats.f_blocks),
+            stats.f_frsize * u64::from(stats.f_bfree),
+        );
         Ok(disk_stats)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-#[macro_use] 
+#[macro_use]
 extern crate quicli;
 use quicli::prelude::*;
 
 extern crate libc;
 
 extern crate ansi_term;
-use ansi_term::Colour::{Red, Yellow, Green};
+use ansi_term::Colour::{Green, Red, Yellow};
 
 mod diskspace;
 use diskspace::*;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
-    #[structopt(short = "h", long = "--human-readable", help = "Print sizes in human readable format (e.g., 1K 234M 2G).")]
+    #[structopt(short = "h", long = "--human-readable",
+                help = "Print sizes in human readable format (e.g., 1K 234M 2G).")]
     human_readable_base2: bool,
-    #[structopt(short = "H", long = "--si", help = "Same as -h, but use powers of 1000 instead of 1024.")]
+    #[structopt(short = "H", long = "--si",
+                help = "Same as -h, but use powers of 1000 instead of 1024.")]
     human_readable_base10: bool,
     mount_point: String,
     #[structopt(long = "verbose", short = "v", parse(from_occurrences))]
@@ -25,11 +27,9 @@ fn format_capacity(capacity: u8) -> String {
     let formatted = format!("{}%", capacity);
     if capacity < 20 {
         Red.paint(formatted).to_string()
-    }
-    else if capacity < 30 {
+    } else if capacity < 30 {
         Yellow.paint(formatted).to_string()
-    }
-    else if capacity > 50 {
+    } else if capacity > 50 {
         Green.paint(formatted).to_string()
     } else {
         formatted
@@ -37,13 +37,19 @@ fn format_capacity(capacity: u8) -> String {
 }
 main!(|args: Cli, log_level: verbosity| {
     let stats = diskspace(args.mount_point.as_str()).unwrap();
-    
-    let base = if args.human_readable_base2 { HUMAN_BASE_1024 } 
-                else if args.human_readable_base10 { HUMAN_BASE_1000 } 
-                else { HUMAN_BASE };
 
-    println!("Size {}, Avail {}, Capacity {}", human_readable_size(stats.used, base), 
-                                                human_readable_size(stats.avail, base) , 
-                                                format_capacity(stats.capacity));
+    let base = if args.human_readable_base2 {
+        HUMAN_BASE_1024
+    } else if args.human_readable_base10 {
+        HUMAN_BASE_1000
+    } else {
+        HUMAN_BASE
+    };
 
+    println!(
+        "Size {}, Avail {}, Capacity {}",
+        human_readable_size(stats.used, base),
+        human_readable_size(stats.avail, base),
+        format_capacity(stats.capacity)
+    );
 });
